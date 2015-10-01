2062258564b0ff9c0d176ce071d068f6faac986c
Andrey <andxbes@gmail.com>
2015-10-01 17:00:56 +0300

    first

---
+++ wp-content/themes/myTheme/footer.php
@@ -0,0 +1,20 @@
+<?php $T_P=  get_template_directory_uri();?>   
+<footer>
+            <h4> Следуйте за нами </h4>
+
+            <div id="anchorSocial" class="content baseSize">
+
+                <a href="#"> <img src="<?=$T_P?>/pic/social/faceBook.png" alt="faceBook"></a>
+                <a href="#"> <img src="<?=$T_P?>/pic/social/twitter40.png" alt="twitter"></a>
+                <a href="#"> <img src="<?=$T_P?>/pic/social/google.png" alt="google"></a>
+                <a href="#"> <img src="<?=$T_P?>/pic/social/in.png" alt="in"></a>
+                <a href="#"> <img src="<?=$T_P?>/pic/social/p.png" alt="p"></a>
+                <a href="#"> <img src="<?=$T_P?>/pic/social/boll.png" alt="bool"></a>
+                <a href="#"> <img src="<?=$T_P?>/pic/social/youtube.png" alt="youtube"></a>
+                <a href="#"> <img src="<?=$T_P?>/pic/social/v.png" alt="v"></a>
+                <a href="#"> <img src="<?=$T_P?>/pic/social/oo.png" alt="oo"></a>
+                <a href="#"> <img src="<?=$T_P?>/pic/social/rss.png" alt="rss"></a>
+
+            </div>
+        </footer>
+
---
+++ wp-content/themes/myTheme/header.php
@@ -0,0 +1,26 @@
+<?php $T_P = get_template_directory_uri();?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Lesson 2</title>
+        <meta charset="UTF-8"/>
+        <link rel="stylesheet" href="<?=$T_P?>/css/l2.css"/>
+        <link rel="stylesheet"   href="<?=$T_P?>/css/screen960.css"/>
+    </head>
+    <body>
+        <header class="fullscrtoWidth">
+
+            <img  id="logo"  src="<?=$T_P?>/pic/logo.png" alt="logo"/>
+
+            <ul class="menuList" >
+                <li> <a href="#">Что мы делаем?</a> </li>
+                <li> <a href="#MeWork">ЗАКАЗАТЬ</a> </li>
+                <li> <a href="#anchoreBenefit">О нас</a> </li>
+                <li> <a href="#anchorWhyUS">Почему мы?</a> </li>
+                <li> <a href="#anchorServicess">Наши товары </a> </li>
+                <li> <a href="#anchorTeam">Наша команда</a> </li>
+                <li> <a href="#anchorSocial">Где нас найти?</a> </li>
+            </ul>
+
+        </header>
+
