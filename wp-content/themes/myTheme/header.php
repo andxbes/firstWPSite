7e34638c9abc49ea823ff554671d0b5de16401aa
Andrey <andxbes@gmail.com>
2015-10-05 13:37:33 +0300

    create page the  We

--- wp-content/themes/myTheme/header.php
+++ wp-content/themes/myTheme/header.php
@@ -12,6 +12,7 @@
         <header class="fullscrtoWidth">
            
 			<a href="/">  <img  id="logo"  src="<?=$T_P?>/pic/logo.png" alt="logo"/></a>
+			<!--<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">-->
 			<?php 
 			   topMenu();
 			?>
