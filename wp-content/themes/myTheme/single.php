134974fffc60b2b30bf234bbc897302331de35ff
Andrey <andxbes@gmail.com>
2015-10-05 11:15:11 +0300

    added  category

--- wp-content/themes/myTheme/single.php
+++ wp-content/themes/myTheme/single.php
@@ -15,30 +15,39 @@ the_post();
 
 
 		<div class = "container">
-			<?php the_title("<h1>","</h1>");?>
-			<?php the_post_thumbnail('large');?>
-			
-			<?php the_content();?>
-		
+			<?php the_title( "<h1>", "</h1>" ); ?>
+			<?php the_post_thumbnail( 'large' ); ?>
+
+			<?php the_content(); ?>
+
 		</div>
+
 		<?php
 		// If comments are open or we have at least one comment, load up the comment template.
 		if ( comments_open() || get_comments_number() ) :
 			comments_template();
 		endif;
-
-		// Previous/next post navigation.
-		the_post_navigation( array(
-			'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
-			'<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
-			'<span class="post-title">%title</span>',
-			'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
-			'<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
-			'<span class="post-title">%title</span>',
-		) );
-
-		// End the loop.
 		?>
+
+		<div class = "container">
+			<div class="row">
+				<div class="col-md-12">
+				<?php
+				// Previous/next post navigation.
+				the_post_navigation( array(
+					'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
+					'<span class="post-title">%title</span>',
+					'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
+					'<span class="post-title">%title</span>',
+				) );
+
+				// End the loop.
+				?>
+				</div>
+			</div>
+		</div>
 </div>
 </main><!-- .site-main -->
 </div><!-- .content-area -->
